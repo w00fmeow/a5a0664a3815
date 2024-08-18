42e4615f8501c18d141a9584c5731ce237ab4cb2
w00fmeow <w00fmeow@protonmail.ch>
2024-08-18 13:11:29 +0300

    - applied formatting

--- a5a0664a3815/csv_extractor.py
+++ a5a0664a3815/csv_extractor.py
@@ -1,14 +1,14 @@
 import csv
 from .file_extractor import FileExtractor
 
-class CSVExtractor(FileExtractor):
-    def __init__(self, has_header_row = True):
-        self.has_header_row = has_header_row
-        super().__init__(['.csv', ".CSV"])
 
-    
+class CSVExtractor(FileExtractor):
+    def __init__(self, has_header_row=True):
+        self.has_header_row = has_header_row
+        super().__init__([".csv", ".CSV"])
+
     def process_file(self, file_path):
-        with open(file_path, mode='r') as file:
+        with open(file_path, mode="r") as file:
             csv_reader = csv.reader(file)
             password_column_index = 0
             email_column_index = 1
@@ -18,15 +18,19 @@ class CSVExtractor(FileExtractor):
 
                 password_column_index = headers.index("password")
                 email_column_index = headers.index("email")
-                
-            for row in csv_reader:
-                self.extract_credentials_from_row(row, password_column_index, email_column_index, file_path)
 
-    def extract_credentials_from_row(self, row, password_column_index, email_column_index, file_path):
+            for row in csv_reader:
+                self.extract_credentials_from_row(
+                    row, password_column_index, email_column_index, file_path
+                )
+
+    def extract_credentials_from_row(
+        self, row, password_column_index, email_column_index, file_path
+    ):
         try:
             password = row[password_column_index]
             email = row[email_column_index]
 
-            super().add_credentials( email,password, file_path)
+            super().add_credentials(email, password, file_path)
         except Exception as e:
             pass
--- a5a0664a3815/sql_extractor.py
+++ a5a0664a3815/sql_extractor.py
@@ -3,15 +3,15 @@ from .file_extractor import FileExtractor
 
 pattern = r"\('([^']*)',[^']*,'([^']*)'"
 
+
 class SQLExtractor(FileExtractor):
     def __init__(self):
-        super().__init__([".sql", '.SQL'])
-        
-   
+        super().__init__([".sql", ".SQL"])
+
     def process_file(self, file_path):
-        with open(file_path, mode='r') as file:
+        with open(file_path, mode="r") as file:
             sql_content = file.read()
-            
+
             matches = re.findall(pattern, sql_content)
 
             for email, password in matches:
@@ -19,6 +19,6 @@ class SQLExtractor(FileExtractor):
 
     def extract_credentials_from_object(self, email, password, file_path):
         try:
-            super().add_credentials( email,password, file_path)
+            super().add_credentials(email, password, file_path)
         except Exception as e:
             pass
